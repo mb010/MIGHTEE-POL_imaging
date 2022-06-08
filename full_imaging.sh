d229879d6828d4aa1b1647b8c7ef23e5ba337035
mb010 <micah.bowles@postgrad.manchester.ac.uk>
2022-06-08 15:59:24 +0100

    full imaging call

--- full_imaging.sh
+++ full_imaging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=30-00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=micah.bowles@postgrad.manchester.ac.uk
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -8,34 +8,44 @@
 #SBATCH --job-name=FullImaging
 #SBATCH --output=./logs/%x.%j.out
 #SBATCH --error=./logs/%x.%j.err
+#SBATCH --exclude=compute-0-8
 
-export VIS=/share/nas2/mbowles/dev/testing/1538856059_sdp_l0.J0217-0449.mms
+#export VIS=/share/nas2/mbowles/dev/testing/1538856059_sdp_l0.J0217-0449.mms #XMMLSS12
+export VIS=/share/nas2/MIGHTEE/calibration/cosmos/1587911796_sdp_l0/1587911796_sdp_l0.4k.J1000+0212.mms #COSMOS test
+#export VIS=/share/nas2/mbowles/dev/1587911796_sdp_l0.4k.J1000+0212_merged.ms
 export CONTAINER=/share/nas2/mbowles/dev/casa-6_v2.simg
 export CHANNEL_WIDTH=2.5078
+export TMP_DIR=/state/partition1/tmp_bowles
 
 export OUTDIR="/share/nas2/mbowles/images/$(basename ${VIS%.*ms})/"
 mkdir $OUTDIR
 
-# Split data into a list of MS
-#SPLIT_ID=$(sbatch ./split/split.sh --export=ALL)
-#SPLIT_ID=${SPLIT_ID##* }
+# Merge SPW of data set to allow for easy channel slicing # This works, but takes >230min (~4hrs)
+IMG_MERGE=$(sbatch --export=ALL ./split/merge_spw.sh)
+IMG_MERGE=${IMG_MERGE##* }
 
-# Imaging:
-# MFS IQUV: 2 briggs weightings
+# Imaging: MFS IQUV, 2 briggs weightings
 IMG_MFS_IQUV1=$(sbatch --export=ALL ./image/image_mfs.sh -0.5)
 IMG_MFS_IQUV1=${IMG_MFS_IQUV1##* }
+sleep 3s
 IMG_MFS_IQUV2=$(sbatch --export=ALL ./image/image_mfs.sh 0.4)
 IMG_MFS_IQUV2=${IMG_MFS_IQUV2##* }
 
-# Channelwise IQUV: 2 briggs weightings
-IMG_CHAN_IQUV1=$(sbatch --export=ALL ./image/image_channels.sh -0.5)
-IMG_CHAN_IQUV1=${IMG_CHAN_IQUV1##* }
-IMG_CHAN_IQUV2=$(sbatch --dependency=afterany:%IMG_CHAN_IQUV1 --export=ALL ./image/image_channels.sh 0.0)
-IMG_CHAN_IQUV2=${IMG_CHAN_IQUV2##* }
+# Change VIS to merged VIS:
+export VIS="${OUTDIR}$(basename ${VIS%.*ms})_merged.ms"
+echo "VIS for channel imaging: ${VIS}"
+sleep 3s
+# Imaging: Channelwise IQUV, 2 briggs weightings
+# Takes approx 5200m (~86h / ~3.5days)
+echo "Waiting for Merge to finish before launching channel imaging."
+IMG_CHAN_IQUV1=$(sbatch --dependency=afterany:$IMG_MERGE --export=ALL ./image/image_channels.sh -0.5 0.0)
+#IMG_CHAN_IQUV1=$(sbatch --export=ALL ./image/image_channels.sh -0.5 0.0)
+IMG_CHAN_IQUV1=${IMG_CHAN_IQUV##* }
 
 # Concatenate images: 2 briggs weightings
 #CONCAT_ID1=$(sbatch --dependency=afterany:$IMG_MFS_IQUV2:$IMG_CHAN_IQUV2 ./concat/concat.sh -0.5)
 #CONCAT_ID2=$(sbatch --dependency=afterany:$IMG_MFS_IQUV2:$IMG_CHAN_IQUV2 ./concat/concat.sh 0.0)
 
 # Cleanup
+# Remove merged VIS during cleanup
 #sbatch --dependency=afterany:$CONCAT_ID1:$CONCAT_ID2 ./cleanup/cleanup.sh
