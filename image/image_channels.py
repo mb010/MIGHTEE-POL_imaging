6472fddbcda1b3ac0cc40ffc6691803fbf406c13
mb010 <micah.bowles@postgrad.manchestser.ac.uk>
2023-03-28 14:23:40 +0100

    IQUV splitting into individual images

--- image/image_channels.py
+++ image/image_channels.py
@@ -227,8 +227,8 @@ def main():
         stokes = ["I"]
     else:
         if parameters["specmode"] == "cube":
-            stokes = ["IQUV"]
-            # stokes = ["I", "Q", "U", "V"]
+            # stokes = ["IQUV"]
+            stokes = ["I", "Q", "U", "V"]
             # nchan = []
             # start = []
         else:
